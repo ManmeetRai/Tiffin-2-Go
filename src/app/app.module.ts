cae748bb7cc1653deb2c0e7efc01b857be1766cf
bhardvis <vishnu4262@gmail.com>
2018-03-10 01:21:10 -0500

    trial changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { UsersComponent } from './users/users.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 
-const appRoutes: Routes = [
-  {path: 'Home', component: HomeComponent},
-  {path: 'Registration', component: RegistrationComponent},
-  {path: 'Users', component: UsersComponent},
-  {path: '', redirectTo: '/Home', pathMatch: 'full'},
-  { path: '**',redirectTo: '/Home', pathMatch: 'full' }
-]
+
 @NgModule({
   declarations: [
-    AppComponent,
-    // RouterModule,
-    RegistrationComponent,
-    UsersComponent,
-    HomeComponent,
-    FooterComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
